bd7c814934c45505be83d8c72f17621a913d7d9a
ToiTL <toitl@rikkeisoft.com>
2016-10-14 17:47:34 +0700

    'change'

--- tester.php
+++ tester.php
@@ -1,67 +1,126 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title></title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
-</head>
-<body>
-<div class="container">
-				<form action="insertlogin.php" method="post">
-					<div class="form-group">
-					<label>User_Id</label>
-					<input type="text" class="form-control" name="userId" value="user_" >
-					</div>
-					<div class="form-group">
-					<label>BundleId</label>
-					<input type="text" class="form-control" name="BundleId" >
-					</div>
-					<div class="form-inline">
-						<div class="form-group">
-							<label>Latitude</label>
-							<input type="text" class="form-control" name= "latitude">
-							</div>
-							<div class="form-group">
-							<label>Longitude</label>
-							<input type="text" class="form-control" name= "longitude" >
-						</div>
-					</div>
-					<div class="form-group">
-					<label>Username</label>
-					<input type="text" class="form-control" name= "username" >
-					</div>
-					 <div class="form-group">
-					<label>Device_type</label>
-					<input type="text" class="form-control" name="device_type" >
-					</div>
-					<div class="form-group">
-					<label>Device_platform</label>
-					<input type="text" class="form-control" name="device_platform" >
-					</div>
-					 <div class="form-group">
-					<label >Device_uid</label>
-					<input type="text" class="form-control" name="device_uid">
-					</div>
-					<div class="form-group">
-					<label >Avatar</label>
-					<input type="text" class="form-control" name="avatar" >
-					</div>
-					<div class="form-group">
-					<label >user_oauthUid</label>
-					<input type="text" class="form-control" name="user_oauthUid" >
-					</div>
-					<div class="form-inline">
-						<div class="form-group">
-							<label >City</label>
-							<input type="text" class="form-control" name="city" >
-							</div>
-							<div class="form-group">
-							<label >Country</label>
-							<input type="text" class="form-control" name="country" >
-						</div>
-					</div>
-					<button type="submit" class="btn btn-default">Submit</button>
-			</form>
-</div>
+	<head>
+		<title>Tester insert Json object</title>
+		<script src="plugins/jquery/jquery.min.js"></script>
+		<style type="text/css">
+			input{
+				width: 50%;
+				margin-bottom: 5px;
+				display: block;
+			}
+		</style>
+	</head>
 
-</body>
+	<body>
+		<div style="float: left; border: 1px solid #ddd; padding: 5px; width: 48%;">
+			<button class="auto">Random logged</button>				
+			<div id="message" style="word-break: break-all;"></div>
+		</div>
+		<div style="float: right; border: 1px solid #ddd; padding: 5px; width: 48%;">
+			<form action="insertlogin.php" method="post">
+				<label>BundleId</label>
+				<input type="text" class="form-control" name="bundleId" >
+
+				<label>User_Id</label>
+				<input type="text" class="form-control" name="user[uid]" value="user_" >
+
+				<label>Username</label>
+				<input type="text" class="form-control" name="user[username]" value="" >
+
+				<label>avatar</label>
+				<input type="text" class="form-control" name="user[avatar]" value="" >
+
+				<label>Latitude</label>
+				<input type="text" class="form-control" name= "coords[latitude]">
+
+				<label>Longitude</label>
+				<input type="text" class="form-control" name= "coords[longitude]" >
+
+				<label>Timestamp</label>
+				<input type="text" class="form-control" name= "coords[timestamp]" >
+
+				<label>Event</label>
+				<input type="text" class="form-control" name= "event[type]" >
+
+				<label>Device type</label>
+				<input type="text" class="form-control" name= "device[type]" >
+
+				<label>Device platform</label>
+				<input type="text" class="form-control" name= "device[platform]" >
+
+				<label>Device uid</label>
+				<input type="text" class="form-control" name= "device[uid]" >
+
+				<button type="submit" class="btn btn-default">Add logged</button>
+			</form>
+			<div id="addmessage" style="word-break: break-all;"></div>
+		</div>
+		<script type="text/javascript">
+		function autoinsert(){
+			var id = Math.floor(Math.random() * 100);
+			var latitude = (Math.random() * (180 + 180) - 180).toFixed(6) * 1;
+			var longitude =(Math.random() * (180 + 180) - 180).toFixed(6) * 1;
+
+			var arrayBundid =['com.akademia.ryokou','overfull.net','google.com','apple.com'];
+
+			var rand = arrayBundid[Math.floor(Math.random() * arrayBundid.length)];
+
+			var data = {
+				bundleId: rand,
+				user: {
+						uid: 'user_'+id,
+						username:'username'+id,
+						avatar: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Map_marker.svg/2000px-Map_marker.svg.png'
+				},
+				device: {
+					type: 'mobile',
+					platform: 'ios',
+					uid: 'device_'
+				},
+				event: {
+					type: 'login',
+				},
+				coords: {
+					latitude: latitude,
+					longitude: longitude,
+					timestamp: '2016-01-25T06:59:59.000Z'
+				}
+			};
+
+			$.ajax({
+				type: "POST",
+				url: 'login.php',
+				data: data,
+				dataType : 'json',
+				success: function(res){
+				  	if(res.error == 0){
+				  		$('#message').append('Added data:<div style="border: 1px solid #ddd; padding: 5px;">' + JSON.stringify(data) + '</div>');
+				  	}
+				}
+			});	
+		}
+		//setInterval(autoinsert, 3000);
+		$('.auto').click(function(){
+			autoinsert();
+		});
+
+		// Set data
+		$('form').submit(function(e){
+			e.preventDefault();
+			var data = $(this).serializeArray();
+			$.ajax({
+				type: "POST",
+				url: 'login.php',
+				data: data,
+				dataType : 'json',
+				success: function(res){
+				  	if(res.error == 0){
+				  		$('#addmessage').append('Added data:<div style="border: 1px solid #ddd; padding: 5px;">' + JSON.stringify(data) + '</div>');
+				  	}
+				}
+			});	
+		});
+		</script>
+	</body>
 </html>
