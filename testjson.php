1808d9b4b483b138eae53d08e1f28668a3fc6344
HoangQuan <quanlybanthan@gmail.com>
2016-10-14 11:53:26 +0700

    change json

--- testjson.php
+++ testjson.php
@@ -8,40 +8,42 @@
 <button type="button" class="btn btn-primary">Insert data</button>
 	<script>
 
-		var id = Math.floor(Math.random() * 100)
-		var data = {
-					bundleId: 'com.akademia.ryokou'+id,
-					user: {
-							uid: 'user_'+id,
-							username:'username'+id,
-							avatar: ''
-					},
-					device: {
-						type: 'mobile',
-						platform: 'ios',
-						uid: 'device_'
-					},
-					event: {
-						type: 'login',
-					},
-					coords: {
-						latitude: 127.3453534,
-						longitude: 30.977961,
-						timestamp: '2016-01-25T06:59:59.000Z'
-					}
-				}
-				
-	$("button").click(function(){
-		$.ajax({
-			type: "POST",
-			url: 'insertjson.php',
-			data: data,
-			dataType : 'json',
-			success: function(res){
-			  alert(res['message']);
-				}
-			});		
-		});
+function autoinsert(){
+				var id = Math.floor(Math.random() * 100);
+				var lat= Math.floor(Math.random() * 100);
+				var lon =Math.floor(Math.random() * 100);
 
+				var data = {
+							bundleId: 'com.akademia.ryokou'+id,
+							user: {
+									uid: 'user_'+id,
+									username:'username'+id,
+									avatar: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Map_marker.svg/2000px-Map_marker.svg.png'
+							},
+							device: {
+								type: 'mobile',
+								platform: 'ios',
+								uid: 'device_'
+							},
+							event: {
+								type: 'login',
+							},
+							coords: {
+								latitude: lat+.027764,
+								longitude: lon+5.834160,
+								timestamp: '2016-01-25T06:59:59.000Z'
+							}
+						};
+				$.ajax({
+					type: "POST",
+					url: 'insertjson.php',
+					data: data,
+					dataType : 'json',
+					success: function(res){
+					  console.log(res['message']);
+						}
+					});	
+		}
+setInterval(autoinsert, 1000*60);
 	</script>
 </html>
