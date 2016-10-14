195c09d42b8ccbb1c432e0c24af17cef9c8e2632
ToiTL <toitl@rikkeisoft.com>
2016-10-14 15:57:03 +0700

    'change-login'

--- login.php
+++ login.php
@@ -42,17 +42,18 @@ try{
 			'bundleid'			=> addslashes($_POST['bundleId'])	,
 			'latitude'			=> addslashes($_POST['coords']['latitude']),
 			'longitude'			=> addslashes($_POST['coords']['longitude']),
-			'timestamp'			=> date('Y-m-d H:i:s'),
+			'timestamp'			=> isset($_POST['coords']['timestamp']) ? date('Y-m-d H:i:s', strtotime($_POST['coords']['timestamp'])) : date('Y-m-d H:i:s'),
 			'username'			=> isset($_POST['user']['username']) ? addslashes($_POST['user']['username']) : '',
 			'device_type'		=> isset($_POST['device']['type']) ? addslashes($_POST['device']['type']) : '',
 			'device_platform'	=> isset($_POST['device']['platform']) ? addslashes($_POST['device']['platform']) : '',
 			'device_uid'		=> isset($_POST['device']['uid']) ? addslashes($_POST['device']['uid']) ? '',
-			'user_oauthUid'		=> '',
+			'user_oauthUid'		=> isset($_POST['user']['oauthUid']) ? addslashes($_POST['user']['oauthUid']) : '',
 			'avatar'			=> isset($_POST['user']['avatar']) ? addslashes($_POST['user']['avatar']) : '',
-			'city'				=> '',
-			'country'			=>''
+			'city'				=> isset($_POST['coords']['city']) ? addslashes($_POST['user']['city']) : '',
+			'country'			=> isset($_POST['coords']['country']) ? addslashes($_POST['user']['country']) : ''
 		];
 
+		// Call method to save data
 		if($model->insertUserLogin($requestData)){
 		 	echo json_encode(['error' => 0, 'message' => 'Success']);
 		 	return;
